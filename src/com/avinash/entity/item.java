94647b1624bc1b76ce54a9ded70ebf948419f48a
avinash322 <54823871+avinash322@users.noreply.github.com>
2019-09-14 11:50:01 +0700

    pbo2-assignment02-20191-avinash322
    ok

---
+++ src/com/avinash/entity/item.java
@@ -0,0 +1,32 @@
+package com.avinash.entity;
+
+public class item {
+    public String nama;
+    public String harga;
+    public Category category;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+}
